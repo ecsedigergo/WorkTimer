d1e5585b249503082bab489ecbda6ff4b1f07524
Ecsedi Gergő <ecsedigergo@gmail.com>
2018-04-09 00:09:51 +0200

    Added ui initial codes for login

---
+++ app/src/main/java/hu/bme/aut/worktimer/ui/login/ILoginScreen.java
@@ -0,0 +1,6 @@
+package hu.bme.aut.worktimer.ui.login;
+
+
+public interface ILoginScreen {
+    void login();
+}
---
+++ app/src/main/java/hu/bme/aut/worktimer/ui/login/LoginPresenter.java
@@ -0,0 +1,8 @@
+package hu.bme.aut.worktimer.ui.login;
+
+import hu.bme.aut.worktimer.ui.Presenter;
+
+
+public class LoginPresenter extends Presenter<ILoginScreen> {
+
+}
