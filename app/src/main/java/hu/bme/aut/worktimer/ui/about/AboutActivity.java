b7aa7349c6703175edccf0452c49e085098ff4e3
Ecsedi Gergő <ecsedigergo@gmail.com>
2018-04-09 01:01:38 +0200

    Added about ui elements

---
+++ app/src/main/java/hu/bme/aut/worktimer/ui/about/AboutActivity.java
@@ -0,0 +1,15 @@
+package hu.bme.aut.worktimer.ui.about;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import hu.bme.aut.worktimer.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
