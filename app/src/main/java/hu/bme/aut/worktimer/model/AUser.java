fbe63f6fc534a898e383ac7bde6a49ec11a02d8f
Ecsedi Gergő <ecsedigergo@gmail.com>
2018-04-08 21:58:50 +0200

    Added model classes

---
+++ app/src/main/java/hu/bme/aut/worktimer/model/AUser.java
@@ -0,0 +1,9 @@
+package hu.bme.aut.worktimer.model;
+
+/**
+ * Abstract class for users in the application
+ * Created by ecsedigergo on 2018. 04. 08..
+ */
+
+public abstract class AUser implements IUser {
+}
---
+++ app/src/main/java/hu/bme/aut/worktimer/model/IUser.java
@@ -0,0 +1,10 @@
+package hu.bme.aut.worktimer.model;
+
+/**
+ * Interface for users in the application
+ * Created by ecsedigergo on 2018. 04. 08..
+ */
+
+public interface IUser {
+
+}
