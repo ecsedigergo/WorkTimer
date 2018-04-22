13a2af82cb377c9e443e47e90a7005791c1fb971
Ecsedi Gergő <ecsedigergo@gmail.com>
2018-04-22 12:41:45 +0200

    added mock classes stub

---
+++ app/src/main/java/hu/bme/aut/worktimer/network/NetworkConfig.java
@@ -0,0 +1,11 @@
+package hu.bme.aut.worktimer.network;
+
+/**
+ * Created by ecsedigergo on 2018. 04. 22..
+ */
+
+public class NetworkConfig {
+    public static final String ENDPOINT_ADDRESS="https://worktimer.hu";
+    public static final String ENDPOINT_PREFIX = "/api/";
+    public static final String SERVICE_ENDPOINT=ENDPOINT_ADDRESS+ENDPOINT_PREFIX;
+}
