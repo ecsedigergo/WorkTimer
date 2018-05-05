05cc5a3e981d21960d452a5a980060e634476f30
Ecsedi Gergő <ecsedigergo@gmail.com>
2018-05-05 15:39:39 +0200

    finished tests

---
+++ app/src/test/java/hu/bme/aut/worktimer/repository/TestRepositoryModule.java
@@ -0,0 +1,15 @@
+package hu.bme.aut.worktimer.repository;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class TestRepositoryModule {
+
+    @Provides
+    public Repository provideRepository() {
+        return new MemoryRepository();
+    }
+}
